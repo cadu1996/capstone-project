d4c62c9cee2059532d7d917a00f97857d00d9dc4
Carlos Eduardo de Souza <carlos.eduardo.gba@gmail.com>
2023-05-01 04:09:37 +0000

    hooks to transfers data from API to S3

---
+++ plugins/transfers/imdb_to_s3.py
@@ -0,0 +1,61 @@
+from airflow.models import BaseOperator
+from airflow.providers.http.hooks.http import HttpHook
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
+class ImdbToS3Operator(BaseOperator):
+    """
+    Operator to transfer data from IMDB to S3.
+    """
+
+    def __init__(
+            self,
+            *,
+            key,
+            bucket_name,
+            endpoint="name.basics.tsv.gz",
+            replace=False,
+            acl_policy=None,
+            imdb_conn_id="imdb_default",
+            s3_conn_id="s3_default",
+            **kwargs
+            ):
+        
+        super().__init__(*args, **kwargs)
+
+        self.key = key
+        self.bucket_name = bucket_name
+        self.endpoint = endpoint
+        self.replace = replace
+        self.acl_policy = acl_policy
+        self.imdb_conn_id = imdb_conn_id
+        self.s3_conn_id = s3_conn_id
+
+
+    def execute(self, context):
+        """
+        Executes the operator.
+        """
+
+        imdb_hook = HttpHook(
+            method="GET",
+            http_conn_id=self.pushshift_conn_id,
+            )
+        
+        s3_hook = S3Hook(
+            aws_conn_id=self.s3_conn_id
+            )
+
+        # Get the pushshift data
+        response = imdb_hook.run(
+            endpoint=self.endpoint,
+        )
+
+        # Upload the data to S3
+        s3_hook.load_bytes(
+            bytes_data=response.content,
+            key=self.key,
+            bucket_name=self.bucket_name,
+            replace=self.replace,
+            acl_policy=self.acl_policy,
+        )
+
